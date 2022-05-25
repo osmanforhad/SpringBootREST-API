6d010c3f2f0fa9de15b11a7df0336d38bf34f684
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-25 16:48:48 +0600

    project setup and create require packages

---
+++ src/test/java/net/osmanforhad/springbootrestapi/SpringbootRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.osmanforhad.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
