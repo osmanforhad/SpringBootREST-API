2140490c6729db6cb1de6af58282f30f30d51c0c
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-25 19:22:57 +0600

    modify the employee model entity and create getallemployee rest api also save some dummy data into db

--- src/main/java/net/osmanforhad/springbootrestapi/SpringbootRestapiApplication.java
+++ src/main/java/net/osmanforhad/springbootrestapi/SpringbootRestapiApplication.java
@@ -1,13 +1,37 @@
 package net.osmanforhad.springbootrestapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.osmanforhad.springbootrestapi.model.Employee;
+import net.osmanforhad.springbootrestapi.repository.EmployeeRepository;
+
 @SpringBootApplication
-public class SpringbootRestapiApplication {
+public class SpringbootRestapiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestapiApplication.class, args);
+	}
+
+	//inject EmployeeRepository class as dependency
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//create Model Entity object
+		Employee employee = new Employee();
+		//set data to save as dummy data
+		employee.setFirstName("Mr");
+		employee.setLastName("osman");
+		employee.setEmailId("osman@gmail.com");
+		
+		//save dummy data into DB through the Repository Interface
+		employeeRepository.save(employee);
+		
 	}
 
 }
---
+++ src/main/java/net/osmanforhad/springbootrestapi/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package net.osmanforhad.springbootrestapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.osmanforhad.springbootrestapi.model.Employee;
+import net.osmanforhad.springbootrestapi.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+	
+	//inject EmployeeRepository class as dependency
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employee
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployee() {
+		return employeeRepository.findAll();
+	}
+
+}
